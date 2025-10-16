e9ef3bec05194a19dfe94a59d2e2242b9fd4c209
xzuyn <16216325+xzuyn@users.noreply.github.com>
2025-10-16 17:15:47 -0400

    add VAEDecodeXZ and VAEDecodeTiledXZ

--- nodes.py
+++ nodes.py
@@ -737,6 +737,81 @@ class TextEncodeQwenImageEditSimpleXZ:
         return (image, conditioning, zeroed_conditioning, {"samples": ref_latent},)
 
 
+class VAEDecodeXZ:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "samples": ("LATENT", {"tooltip": "The latent to be decoded."}),
+                "vae": ("VAE", {"tooltip": "The VAE model used for decoding the latent."}),
+                "disable_cudnn": ("BOOLEAN", { "default": True}),
+            }
+        }
+    RETURN_TYPES = ("IMAGE",)
+    OUTPUT_TOOLTIPS = ("The decoded image.",)
+    FUNCTION = "decode"
+
+    CATEGORY = "xzuynodes"
+    DESCRIPTION = "Decodes latent images back into pixel space images."
+
+    def decode(self, vae, samples, disable_cudnn=True):
+        if disable_cudnn:
+            torch.backends.cudnn.enabled = False
+
+        images = vae.decode(samples["samples"])
+        
+        if disable_cudnn:
+            torch.backends.cudnn.enabled = True
+
+        if len(images.shape) == 5: #Combine batches
+            images = images.reshape(-1, images.shape[-3], images.shape[-2], images.shape[-1])
+
+        return (images, )
+
+
+class VAEDecodeTiledXZ:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {"samples": ("LATENT", ), "vae": ("VAE", ),
+                             "tile_size": ("INT", {"default": 512, "min": 64, "max": 4096, "step": 32}),
+                             "overlap": ("INT", {"default": 64, "min": 0, "max": 4096, "step": 32}),
+                             "temporal_size": ("INT", {"default": 64, "min": 8, "max": 4096, "step": 4, "tooltip": "Only used for video VAEs: Amount of frames to decode at a time."}),
+                             "temporal_overlap": ("INT", {"default": 8, "min": 4, "max": 4096, "step": 4, "tooltip": "Only used for video VAEs: Amount of frames to overlap."}),
+                             "disable_cudnn": ("BOOLEAN", { "default": True}),
+                            }}
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "decode"
+
+    CATEGORY = "xzuynodes"
+
+    def decode(self, vae, samples, tile_size, overlap=64, temporal_size=64, temporal_overlap=8, disable_cudnn=True):
+        if tile_size < overlap * 4:
+            overlap = tile_size // 4
+        if temporal_size < temporal_overlap * 2:
+            temporal_overlap = temporal_overlap // 2
+        temporal_compression = vae.temporal_compression_decode()
+        if temporal_compression is not None:
+            temporal_size = max(2, temporal_size // temporal_compression)
+            temporal_overlap = max(1, min(temporal_size // 2, temporal_overlap // temporal_compression))
+        else:
+            temporal_size = None
+            temporal_overlap = None
+
+        if disable_cudnn:
+            torch.backends.cudnn.enabled = False
+
+        compression = vae.spacial_compression_decode()
+        images = vae.decode_tiled(samples["samples"], tile_x=tile_size // compression, tile_y=tile_size // compression, overlap=overlap // compression, tile_t=temporal_size, overlap_t=temporal_overlap)
+
+        if disable_cudnn:
+            torch.backends.cudnn.enabled = True
+
+        if len(images.shape) == 5: #Combine batches
+            images = images.reshape(-1, images.shape[-3], images.shape[-2], images.shape[-1])
+
+        return (images, )
+
+
 NODE_CLASS_MAPPINGS = {
     "FirstLastFrameXZ": FirstLastFrameXZ,
     "ImageResizeKJ": ImageResizeKJ,
@@ -748,6 +823,8 @@ NODE_CLASS_MAPPINGS = {
     "WanImageToVideoXZ": WanImageToVideoXZ,
     "TextEncodeQwenImageEditXZ": TextEncodeQwenImageEditXZ,
     "TextEncodeQwenImageEditSimpleXZ": TextEncodeQwenImageEditSimpleXZ,
+    "VAEDecodeXZ": VAEDecodeXZ,
+    "VAEDecodeTiledXZ": VAEDecodeTiledXZ,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FirstLastFrameXZ": "First/Last Frame (XZ)",
@@ -760,4 +837,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "WanImageToVideoXZ": "WanImageToVideo (XZ)",
     "TextEncodeQwenImageEditXZ": "TextEncodeQwenImageEdit (XZ)",
     "TextEncodeQwenImageEditSimpleXZ": "TextEncodeQwenImageEditSimple (XZ)",
+    "VAEDecodeXZ": "VAEDecode (XZ)",
+    "VAEDecodeTiledXZ": "VAEDecodeTiled (XZ)",
 }
